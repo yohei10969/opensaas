b217e134c72494b275d68ea737a8eb76fc1d9805
Yohei Fujii <yoheifujii415@gmail.com>
2025-04-22 08:49:00 +0900

    create file

---
+++ database/migrations/individuals/2025_04_21_084813_create_individuals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /**
+         * これはユーザー認証に関するテーブルです。
+         * 個人情報（プロフィール）に関しては Personals テーブルを参照。
+         * 
+         */
+        Schema::create('individuals', function (Blueprint $table) {
+            $table->string('uuid', 36)->primary();
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('individuals');
+    }
+};
