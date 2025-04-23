461ec996c76d6241321e1a457faf686cd189b72a
Yohei Fujii <yoheifujii415@gmail.com>
2025-04-23 13:19:50 +0900

    x

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign</title>
+</head>
+<body>
+    <form action="" method="post">
+        @csrf
+        <input type="email" name="email" id="">
+        <input type="password" name="password" id="">
+    </form>
+</body>
+</html>
