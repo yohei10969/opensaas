0e3a30a0d12ce23f9d45ddf978acb7dcf3ad25f3
Yohei Fujii <yoheifujii415@gmail.com>
2025-04-24 15:44:05 +0900

    create

---
+++ database/migrations/accounts/2025_04_24_052435_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->string('uuid', 36)->primary(); // uuid
+
+            $table->string('alphabet', 100)->nullable(); // アカウント名・アルファベット
+            $table->string('japanese', 100)->nullable(); // アカウント名・日本語
+
+            $table->foreignUuid('administrator') // 管理者・UUID型の外部キーカラムを作成
+                  ->constrained('individuals', 'uuid'); // individualsテーブルのuuidカラムを参照
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
