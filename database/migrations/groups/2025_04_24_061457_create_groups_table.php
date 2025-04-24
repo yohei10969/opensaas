cbf15887d438a79aa04349e0f539e4a3584f51ef
Yohei Fujii <yoheifujii415@gmail.com>
2025-04-25 08:41:11 +0900

    Initial Concept

--- database/migrations/groups/2025_04_24_061457_create_groups_table.php
+++ database/migrations/groups/2025_04_24_061457_create_groups_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('groups', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('uuid')->primary(); // uuid
+            $table->string('name', 100); // グループ名
+
+            $table->foreignUuid('accounts_uuid') // 所属アカウント・UUID型の外部キーカラムを作成
+            ->constrained('accounts', 'uuid') // accountsテーブルのuuidカラムを参照 (外部キー)
+            ->nullable()
+            ->onDelete('cascade'); // 親(accounts)が削除されたら子(groups)も削除
+            
             $table->timestamps();
         });
     }
