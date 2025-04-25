87bba560c1c6b4fcdc3d06901493702e80971651
Yohei Fujii <yoheifujii415@gmail.com>
2025-04-25 14:09:25 +0900

    Initial configuration

--- database/migrations/members/2025_04_24_064446_create_members_table.php
+++ database/migrations/members/2025_04_24_064446_create_members_table.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('members', function (Blueprint $table) {
-            $table->id();
+            $table->foreignUuid('individuals_uuid') // UUID型の外部キーカラムを作成
+                  ->constrained('individuals', 'uuid') // individualsテーブルのuuidカラムを参照
+                  ->onDelete('cascade'); // 親(individuals)が削除されたら子(members)も削除
+
+            // アカウント
+            $table->foreignUuid('accounts_uuid') // UUID型の外部キーカラムを作成
+                  ->constrained('accounts', 'uuid') // accountsテーブルのuuidカラムを参照
+                  ->onDelete('cascade'); // 親(accounts)が削除されたら子(members)も削除
+            
             $table->timestamps();
+            $table->primary(['individuals_uuid', 'accounts_uuid']);
         });
     }
 
