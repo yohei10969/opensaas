8f3ae1312e5b777e30d4e5a2eb1cb3af2b2e6c75
Yohei Fujii <yoheifujii415@gmail.com>
2025-04-30 19:02:39 +0900

    Change the redirect destination when authentication expires

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // 認証されていないユーザーのリダイレクト先を 'signin' ルートに設定
+        $middleware->redirectGuestsTo(fn (Request $request) => route('signin'));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
